d424c6536f755aa1770125b6079198cb27a2b47b
이해인 <maetdori@daou.co.kr>
2022-08-02 14:31:39 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/nettytransfile/NettyTransfileApplication.java
@@ -0,0 +1,13 @@
+package com.nettytransfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyTransfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyTransfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nettytransfile/NettyTransfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nettytransfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyTransfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
